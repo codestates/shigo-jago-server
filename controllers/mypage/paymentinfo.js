db3e4764c6ad1dcbeabf4a5583ca877e77063882
urusara13 <urusara13@gmail.com>
2020-12-24 11:24:06 +0900

    [API] paymentinfo paymentinfo 테스트 완료 paymentinfo가 get에서 post로 변경됨 /routes/mypage.js : paymentinfo get -> post

--- controllers/mypage/paymentinfo.js
+++ controllers/mypage/paymentinfo.js
@@ -1,3 +1,45 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const { Payment } = require('../../models')
+
+// {
+//     "data": {
+//               "price": "price",
+//               "howToPaid": "howToPaid",
+//               "cardNumber": "cardNumber"
+//             },
+            
+//     "message": "ok"
+// }
+
+//401 { "error": "not authorized" }
 module.exports = async (req, res) => {
+
+  const authorization = req.headers.authorization
+
+  if(authorization === undefined) {
+    res.status(401).json({
+      error: 'not authorized'
+    })
+  }
+  else {
+
+    const token = authorization.split('Bearer ')[1]
+    const data = jwt.verify(token, process.env.ACCESS_SECRET)
+
+    const paymentInfo = await Payment.findOne({
+        where: { reservationId: req.body.reservationId },
+      })
     
+    let obj = paymentInfo.dataValues
+    delete obj.updatedAt
+    delete obj.ReservationId
+
+
+    res.status(201).json({ 
+    "data":  obj,
+    "message": "ok" 
+    })
+
+  }
 }
--- routes/mypage.js
+++ routes/mypage.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { mypageController } = require("../controllers")
 
-router.get("/paymentinfo", mypageController.paymentinfo)
+router.post("/paymentinfo", mypageController.paymentinfo)
 router.get("/reserveinfo", mypageController.reserveinfo)
 router.get("/userinfo", mypageController.userinfo)
 router.post("/writereview", mypageController.writereview)

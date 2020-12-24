b33621f215303d6dfe9f908f6c1941701ea6c859
unow30 <yunho7205@gmail.com>
2020-12-24 11:18:20 +0900

    [API] user/signup
    Assignee : 이름
    
    Job Description
    
    >엔드포인트 search/list 구현하기
    gitbook에 작성된 request, response를 구현

--- controllers/user/signup.js
+++ controllers/user/signup.js
@@ -1,3 +1,25 @@
+const { User } = require("../../models")
+require("dotenv").config();
 module.exports = async (req, res) => {
-    
+    const { email, name, password, mobile } = req.body;
+
+    if (!email || !password || !name || !mobile) {
+        res.status(422).send({ "error": "insufficient parameters supplied" })
+    }
+    const [userInfo, created] = await User.findOrCreate({
+        where: {
+            email: email
+        },
+        defaults: {
+            name: name,
+            password: password,
+            mobile: mobile
+        }
+    })
+    if (created) {
+        res.status(201).send({ "message": "successfully signed up!" })
+    } else {
+        res.status(409).send({ "error": "email exists" })
+    }
+
 }

c0fc15385b18c8d8c632869f373fc5aaee2dfdbc
unow30 <yunho7205@gmail.com>
2020-12-24 23:05:02 +0900

    [API] user/logout
    Assignee : 이름
    
    Job Description
    작업 내용을 적어주세요
    
    엔드포인트 user/logout 구현하기
    gitbook에 작성된 request, response를 구현
    authorization기능 추가

--- controllers/user/logout.js
+++ controllers/user/logout.js
@@ -1,9 +1,17 @@
 module.exports = async (req, res) => {
-    if (!req.cookies.refreshToken) {
-        res.status(400).send({ "error": "you're currently not logined" })
+
+    const authorization = req.headers.authorization
+    if (authorization === undefined) {
+        res.status(400).json({
+            "error": "you're currently not logined"
+        });
+
+    } else if (!req.cookies.refreshToken) {
+        res.status(400).json({ "error": "you're currently not logined" })
     } else {
         res.clearCookie('refreshToken');
-        res.status(200).send({ "message": "successfully signed out!" });
+        req.headers.authorization = null
+        res.status(200).json({ "message": "successfully signed out!" });
     }
 
 }

f7a506aa653068e8f91d4a4b29f4c905d1c0eba3
urusara13 <urusara13@gmail.com>
2020-12-25 11:42:05 +0900

    [API] search/list search list 작성완료 config.js, github/issue_template 삭제

--- controllers/search/list.js
+++ controllers/search/list.js
@@ -1,3 +1,37 @@
+require('dotenv').config()
+const axios = require('axios')
+
 module.exports = async (req, res) => {
+     
+    const { areaCode, sigunguCode } = req.body
     
+    const url = `http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchStay?ServiceKey=${process.env.API_SECRET}&areaCode=${areaCode}&sigunguCode=${sigunguCode}&listYN=Y&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&arrange=A&numOfRows=12&pageNo=1&_type=json`
+    
+    const hotels = await axios.get(url)
+
+    const hotelList = hotels.data.response.body.items.item
+
+    const data = []
+    hotelList.forEach(obj => {
+        let newObj = Object.assign({}, {
+            addr1 : obj.addr1,
+            addr2 : obj.addr2,
+            areacode: obj.areacode,
+            sigungucode: obj.sigungucode,
+            contentid: obj.contentid,
+            contenttypeid: obj.contenttypeid,
+            image1 : obj.firstimage,
+            image2 : obj.firstimage2,
+            readcount: obj.readcount,
+            mobile: obj.tel,
+            title: obj.title
+        })
+        data.push(newObj)
+    })
+
+    res.status(201).json({
+        data: data,
+        message: "ok"
+    })
+
 }

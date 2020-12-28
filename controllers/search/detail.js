552a2f7a2d9128db8578ac3220cda1526c631cb5
urusara13 <urusara13@gmail.com>
2020-12-28 13:42:38 +0900

    [API] search/detail search/detail: body의 2가지 값으로 tourAPI 단일쿼리 search/list: console.log 삭제

--- controllers/search/detail.js
+++ controllers/search/detail.js
@@ -1,3 +1,13 @@
+require('dotenv').config()
+const axios = require('axios')
+
 module.exports = async (req, res) => {
     
+    const { contentid, contenttypeid } = req.body
+    
+    const url = `http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailCommon?ServiceKey=${process.env.API_SECRET}&contentTypeId=${contenttypeid}&contentId=${contentid}&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&transGuideYN=Y&_type=json`
+    
+    const hotelinfo = await axios.get(url)
+
+    res.status(200).json(hotelinfo.data.response.body.items.item)
 }

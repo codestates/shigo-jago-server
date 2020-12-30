7732b5c767a55f033a535b60213a64d0559e0162
unow30 <yunho7205@gmail.com>
2020-12-30 19:15:02 +0900

    [API] mypage/reservationInfo modify
    true, false 정확히 전달

--- controllers/social/google/callback.js
+++ controllers/social/google/callback.js
@@ -1,3 +1,19 @@
+require('dotenv').config()
+const axios = require('axios')
+
 module.exports = async (req, res) => {
-    res.status(200).send('test')
+    const { authorizationCode, isDelete } = req.body
+
+    if (authorizationCode) {
+        const url = `https://accounts.google.com/o/oauth2/token?code=${authorizationCode}&client_id=88270045748-rpghv716gefg0loa2t073mr09qv7ej8l.apps.googleusercontent.com&client_secret=X_qTkfsV_ff4xpvOTJ0AjdGd&redirect_uri=http://localhost:3000/google&grant_type=authorization_code`
+        const googleAcctoken = await axios.post(url)
+        res.status(200).json({
+            data: googleAcctoken.data,
+            message: "ok"
+        })
+
+    }
+    else res.status(400).json({
+        error: 'something error'
+    })
 }

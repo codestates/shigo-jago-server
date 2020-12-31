f6f338e2d1c84e535373ace5024223a56ef712fe
urusara13 <urusara13@gmail.com>
2020-12-31 15:13:15 +0900

    hotfix detail/review search/list

--- controllers/detail/review.js
+++ controllers/detail/review.js
@@ -7,8 +7,9 @@ module.exports = async (req, res) => {
     const { contenttypeId, contentId } = req.body
     
     const url = `http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailCommon?ServiceKey=${process.env.API_SECRET}&contentTypeId=${contenttypeId}&contentId=${contentId}&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y&transGuideYN=Y&_type=json`
-    
+
     const hotel = await axios.get(url)
+
     const title = hotel.data.response.body.items.item.title
 
     const hotelInfo = await Hotel.findOne({
@@ -16,14 +17,22 @@ module.exports = async (req, res) => {
       where: { hotelname: title },
     })
 
-    const review = await Review.findAll({
-      raw: true,
-      where: { hotelId: hotelInfo.id}
-    })
-
-    res.status(201).json({ 
-        "data":  review,
+    if(hotelInfo) {
+      const review = await Review.findAll({
+        raw: true,
+        where: { hotelId: hotelInfo.id}
+      })
+  
+      res.status(201).json({ 
+          "data":  review,
+          "message": "ok" 
+        })
+    }
+    
+    else {
+      res.status(201).json({ 
+        "data":  [],
         "message": "ok" 
       })
-
+    }
 }
--- controllers/search/list.js
+++ controllers/search/list.js
@@ -12,31 +12,57 @@ module.exports = async (req, res) => {
 
 
     const hotelList = hotels.data.response.body.items.item
-    console.log(hotelList)
-    
-    const data = []
-    hotelList.forEach(obj => {
-        let newObj = Object.assign({}, {
-            addr1 : obj.addr1,
-            addr2 : obj.addr2,
-            areacode: obj.areacode,
-            sigungucode: obj.sigungucode,
-            contentid: obj.contentid,
-            contenttypeid: obj.contenttypeid,
-            image1 : obj.firstimage,
-            image2 : obj.firstimage2,
-            readcount: obj.readcount,
-            mobile: obj.tel,
-            title: obj.title,
-            mapx: obj.mapx,
-            mapy: obj.mapy
+    console.log(hotelList) //배열이 아닐경우
+    if(Array.isArray(hotelList)) {
+        const data = []
+        hotelList.forEach(obj => {
+            let newObj = Object.assign({}, {
+                addr1 : obj.addr1,
+                addr2 : obj.addr2,
+                areacode: obj.areacode,
+                sigungucode: obj.sigungucode,
+                contentid: obj.contentid,
+                contenttypeid: obj.contenttypeid,
+                image1 : obj.firstimage,
+                image2 : obj.firstimage2,
+                readcount: obj.readcount,
+                mobile: obj.tel,
+                title: obj.title,
+                mapx: obj.mapx,
+                mapy: obj.mapy
+            })
+            data.push(newObj)
         })
-        data.push(newObj)
-    })
+    
+        res.status(201).json({
+            data: data,
+            message: "ok"
+        })
+    }
 
-    res.status(201).json({
-        data: data,
-        message: "ok"
-    })
+    else {
+        let arr = []
+        let newObj = Object.assign({}, {
+            addr1: hotelList.addr1,
+            addr2: hotelList.addr2,
+            areacode: hotelList.areacode,
+            sigungucode: hotelList.sigungucode,
+            contentid: hotelList.contentid,
+            contenttypeid: hotelList.contenttypeid,
+            image1: hotelList.firstimage,
+            image2: hotelList.firstimage2,
+            readcount: hotelList.readcount,
+            mobile: hotelList.tel,
+            title: hotelList.title,
+            mapx: hotelList.mapx,
+            mapy: hotelList.mapy
+        })
+        arr.push(newObj)
+        res.status(201).json({
+            data: arr,
+            message: "ok"
+        })
+    }
+
 
 }
